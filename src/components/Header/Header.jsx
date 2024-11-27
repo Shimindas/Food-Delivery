a4470e4c438b2ef6fdc9c30c07b3a5ce20c87a1b
Shimin Das <shimindas2002@gmail.com>
2024-11-27 18:35:54 +0530

    route,home,cart,placeorder

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-components'>
+        <h2>Oredr your favourite food here</h2>
+        <p>We believe that everyone deserves delicious, wholesome food without the hassle of cooking. Our Tiffin service brings you the essence of home cooking, using only the freshest ingredients and authentic recipes.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
